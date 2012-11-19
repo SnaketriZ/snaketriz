ff3f98afb1ad621712e1b7a92bab9013e6eeaf95
Lokaler Benutzer <Lokaler Benutzer@C-26-13.Schule.107842.mts-schule.de>
2012-11-19 10:18:19 +0100

    ,µ

--- Snaketriz/src/Model/Powerup.java
+++ Snaketriz/src/Model/Powerup.java
@@ -5,9 +5,32 @@ import Enums.PowerupTyp;
 public class Powerup implements Runnable{
 	private PowerupTyp typ;
 	private Snaketriz snaketriz;
-
+	
+	public Powerup(String powerUpTyp){
+		switch (PowerupTyp.valueOf(powerUpTyp)) {
+		case OWN_MOVE_FAST:			
+			break;
+		case OWN_MOVE_SLOW:
+			break;
+		case ENEMY_MOVE_FAST:
+			break;
+		case ENEMY_MOVE_SLOW:
+			break;
+		case TABOO:
+			break;
+		case CONTROL_SWITCH:
+			break;
+		case RANDOM:
+			break;
+		case CHAINSAW:
+			break;
+		default:
+			break;
+		}
+	}
+	
 	private void generate() {
-
+		
 	}
 
 	private void start() {
